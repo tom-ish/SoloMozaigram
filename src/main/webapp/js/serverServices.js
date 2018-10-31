6aae42c45c93ac2ee1d579675372556d3a28797c
tom-ish <tomohiro.ishiwata@gmail.com>
2018-10-31 07:02:35 +0100

    handle login error page

--- src/main/webapp/js/serverServices.js
+++ src/main/webapp/js/serverServices.js
@@ -67,9 +67,10 @@ var ServerServices = {
 					console.log(textStatus);
 					console.log(jqXHR.responseText + " status: " + jqXHR.status);
 					//alert("Erreur Ajax: Connexion is not working.\n" + textStatus + " " + errorThrown);
-					$('#error').innerHTML = "" + jqXHR.responseText;
-					$('#container').hide();
-					$('#error').show();
+					
+					document.open();
+					document.write(jqXHR.reponseText);
+					document.close();
 				}
 			});
 		},
