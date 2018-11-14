53cef6b0469b8bb8ef04f2649d8c7106d859e18d
tom-ish <tomohiro.ishiwata@gmail.com>
2018-11-14 22:07:47 +0100

    jquery $ bug

--- src/main/webapp/js/myspace.js
+++ src/main/webapp/js/myspace.js
@@ -1,8 +1,15 @@
 var mySpaceContent = {
-	initialize : function(){			
+	initialize : function(){
+		if($('ui.labeled.icon.sidebar') == undefined)
+			console.log("jquery $$$ not working");
+		else  {
+			console.log("jquery $$$ working \n");
+			console.log($('ui.labeled.icon.sidebar'));
+		}
+		
 		$('ui.labeled.icon.sidebar').sidebar('toggle');
 		var page_owner = localStorage.getItem("username");
-		$('#usernameDiv').innerHTML = "Welcome " + page_owner;
+		document.getElementById("usernameId").innerHTML = "Welcome " + page_owner;
 	}
 };
 
